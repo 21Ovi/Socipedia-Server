f92ebe5c4cf218f794a99c8981078fec2808fd33
= <=>
2022-12-20 01:06:19 +0530

    registration authentication added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 
-import { register } from "./controllers/auth";
+import { register } from "./controllers/auth.js";
 
 // CONFIGURATIONS
 const __filename = fileURLToPath(import.meta.url);
@@ -37,7 +37,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 // ROUTES WITH FILES
-app.post("/auth/register", upload.single("picture"), register);
+app.post("/auth/register", upload.single("picture"));
 
 // /MONGOOSE SETUP
 const PORT = process.env.PORT || 6001;
