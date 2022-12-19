98afad787fdb608393e706c1610825b606012f2d
= <=>
2022-12-20 00:31:10 +0530

    server running

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 // CONFIGURATIONS
-const __file = fileURLToPath(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
@@ -33,3 +33,15 @@ const storage = multer.diskStorage({
   },
 });
 const upload = multer({ storage });
+
+// /MONGOOSE SETUP
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`server ruuning on Port : ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
