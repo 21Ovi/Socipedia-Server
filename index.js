c976ea783a59657bc9ef8a5f8801b717e4c4c045
= <=>
2022-12-24 22:59:59 +0530

    image bug fixed

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use("/asstes", express.static(path.join(__dirname, "public/assets")));
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 // FILE STORAGE
 const storage = multer.diskStorage({
